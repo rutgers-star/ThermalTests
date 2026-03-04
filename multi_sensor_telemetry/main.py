e48d415565b4b8af1ddf296938a26eb18d16b831
Aaron <aaronchu022@gmail.com>
2026-03-04 18:40:10 -0500

    need to test

--- multi_sensor_telemetry/main.py
+++ multi_sensor_telemetry/main.py
@@ -1,35 +1,65 @@
 import argparse
+from multi_tmpsensor import MultiTMPSensors
 from tmpsensor import TMPSensor
 from fake_tmpsensor import FakeTMPSensor
 import smbus
 import RPi.GPIO as GPIO
 import time
 
-
+'''main.py --single --address 0x4f  main.py --single --fake'''
 def main():
 
-    ADDRESS = 0x4f
     TEMP_REG = 0x00
     CONFIG_REG = 0x01
     TLOW_REG = 0x10
     HIGH_REG = 0x11
     CHANNEL = 1
 
-    bus = smbus.SMBus(CHANNEL)
-    bus.write_byte_data(ADDRESS, 0x01, 0b1100000)
-
-    testtmp = TMPSensor(addr=0x4f, bus = None, reg = 0x00, interval=0.5)
-    testtmp.start()
-
-    testfake = FakeTMPSensor(addr=0x00, reg=0x00, interval=0.5, baseline=70.0)
-    testfake.start()
-    
     parser = argparse.ArgumentParser()
-
+    parser.add_argument("--address", type=lambda x: int(x, 0), default=0x4f, help="I2C address of sensor (e.g. 0x4f or 79)")
+    parser.add_argument("--fake", action="store_true", help="Use fake sensors")
+    parser.add_argument("--single", action="store_true", help="Run single TMPSensor + FakeTMPSensor instead of MultiTMPSensors")
     args = parser.parse_args()
+    ADDRESS = args.address
 
+    if args.single:
+        if not args.fake:
+            bus = smbus.SMBus(CHANNEL)
+            bus.write_byte_data(ADDRESS, CONFIG_REG, 0b1100000)
+            testtmp = TMPSensor(addr=ADDRESS, bus=bus, reg=TEMP_REG, interval=0.5)
+            testtmp.start()
+        testfake = FakeTMPSensor(addr=0x00, reg=TEMP_REG, interval=0.5, baseline=70.0)
+        testfake.start()
+        try:
+            while True:
+                if not args.fake:
+                    print("real:", testtmp.get_latest(), "fake:", testfake.get_latest())
+                else:
+                    print("fake:", testfake.get_latest())
+                time.sleep(1)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            if not args.fake:
+                testtmp.stop()
+                bus.close()
+            testfake.stop()
+    else:
+        if not args.fake:
+            bus = smbus.SMBus(CHANNEL)
+            bus.write_byte_data(ADDRESS, CONFIG_REG, 0b1100000)
+            bus.close()
+        m = MultiTMPSensors(use_fake=args.fake)
+        m.start()
+        try:
+            while True:
+                print(m.get_latest())
+                time.sleep(1)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            m.stop()
 
 
 if __name__ == "__main__":
-
     main()
--- multi_sensor_telemetry/multi_tmpsensor.py
+++ multi_sensor_telemetry/multi_tmpsensor.py
@@ -1,26 +1,77 @@
 from tmpsensor import TMPSensor
 from fake_tmpsensor import FakeTMPSensor
 import threading
+import csv
+import os
+import smbus
 
 class MultiTMPSensors:
 
     def __init__(self, use_fake=False):
-        pass
+        self.use_fake = use_fake
+        self._sensors = []
+        self._bus = None
 
     def get_addrs(self):
-        pass
+        addrs = []
+        bus = smbus.SMBus(1)
+        for addr in range(0x48, 0x50):
+            try:
+                bus.read_i2c_block_data(addr, 0x00, 2)
+                addrs.append(addr)
+            except (OSError, IOError):
+                pass
+        bus.close()
+        return addrs
 
     def get_sensors(self):
-        pass
+        return list(self._sensors)
 
     def start(self):
-        pass
+        if not self._sensors:
+            if self.use_fake:
+                for addr in range(0x48, 0x4C):
+                    self._sensors.append(FakeTMPSensor(addr=addr, reg=0x00, interval=0.5, baseline=70.0))
+            else:
+                addrs = self.get_addrs()
+                self._bus = smbus.SMBus(1)
+                for addr in addrs:
+                    self._sensors.append(TMPSensor(addr=addr, bus=self._bus, reg=0x00, interval=0.5))
+        for s in self._sensors:
+            s.start()
 
     def stop(self):
-        pass
+        sensors = list(self._sensors)
+        self._sensors = []
+        for s in sensors:
+            s.stop()
+        if self._bus is not None:
+            try:
+                self._bus.close()
+            except Exception:
+                pass
+            self._bus = None
 
     def get_latest(self):
-        pass
+        out = {}
+        for s in self._sensors:
+            t = s.get_latest()
+            if t is not None:
+                out[hex(s.addr)] = t
+        return out
 
     def write_csv(self, rows, filename=None):
-        pass
+        if filename is None:
+            filename = "multi_tmp100_log.csv"
+        if not rows:
+            return
+        write_header = not os.path.exists(filename) or os.path.getsize(filename) == 0
+        with open(filename, "a", newline="") as f:
+            if isinstance(rows[0], dict):
+                w = csv.DictWriter(f, fieldnames=rows[0].keys())
+                if write_header:
+                    w.writeheader()
+                w.writerows(rows)
+            else:
+                w = csv.writer(f)
+                w.writerows(rows)
