b0932b29a950d166afabb905a675b51d656d7154
Aaron <aaronchu022@gmail.com>
2026-03-25 18:39:17 -0400

    update

--- multi_sensor_telemetry/tmpsensor.py
+++ multi_sensor_telemetry/tmpsensor.py
@@ -46,7 +46,4 @@ class TMPSensor:
     def get_latest(self):
         return self._latest
     
-<<<<<<< HEAD
-=======
     ## only prints one value
->>>>>>> 456571bab87a6c49926cd22ae34cf054a86068e7
